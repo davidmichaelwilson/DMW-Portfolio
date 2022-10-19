d7a443a3aeb2337e72e189d875de70aa26e09ab7
David M. Wilson <davidmichaelwilson81@gmail.com>
2022-10-19 14:03:40 -0500

    Completed NAV, beginning Header

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -8,12 +8,12 @@ import './Navbar.scss';
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
-    <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
 
-      <ul className='app__navbar-links'>
+      <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
             <li className='app__flex p-text' key={`link-${item}`}>
               <div />
@@ -27,7 +27,7 @@ const Navbar = () => {
             {toggle && (
               <motion.div
                 whileInView={{x: [300,0] }}
-                transtion={{duration: 0.85, ease: 'easeOut'}}
+                transition={{duration: 0.85, ease: 'easeOut'}}
               >
                 <HiX onClick={() => setToggle(false)} />
                   <ul>
